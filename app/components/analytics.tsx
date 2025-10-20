3468790427799ee82bf7a18649e89007b2091fed
Yash Narayan <yash25.narayan@gmail.com>
2025-10-20 10:46:07 -0400

    Update analytics import to use @vercel/analytics/next

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function ClientAnalytics() {
